0bbf4055f8cdb742d3789ab57b963bb867d1abe0
FatimaElhaddad <116873467+FatimaElhaddad@users.noreply.github.com>
2023-01-21 02:00:05 +0100

    Add files via upload

---
+++ bibliotheque.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class bibliotheque
+    {
+        private string Livre;
+        private string Employé;
+
+        public bibliotheque(string livre, string employé)
+        {
+            Livre = livre;
+            Employé = employé;
+        }
+    }
+}
