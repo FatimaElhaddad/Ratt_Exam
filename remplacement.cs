dbb17f66e841b60309b66497e605c6d71b68efba
FatimaElhaddad <116873467+FatimaElhaddad@users.noreply.github.com>
2023-01-21 02:06:32 +0100

    Add files via upload

---
+++ remplacement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class remplacement
+    {
+        private int ID;
+        private int Nom;
+
+        public remplacement(int id, int nom)
+        {
+            ID = id;
+            Nom = nom;
+        }
+    }
+}
