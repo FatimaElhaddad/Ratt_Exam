63f8ba6d594b42a3119d04785ae97003b8efa79c
FatimaElhaddad <116873467+FatimaElhaddad@users.noreply.github.com>
2023-01-21 02:06:55 +0100

    Add files via upload

---
+++ livre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class livre
+    {
+        private int ID;
+        private string Nom;
+
+        public livre(int id, string nom)
+        {
+            ID = id;
+            Nom = nom;
+        }
+        public void commande()
+        {
+        }
+        public void reception() 
+        {
+        }
+        public void deteriration()
+        {
+        }
+    }
+}
