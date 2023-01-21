997bbe9b84cebde28827bbb9939e328fbb996494
FatimaElhaddad <116873467+FatimaElhaddad@users.noreply.github.com>
2023-01-21 02:07:21 +0100

    Add files via upload

---
+++ Employe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class Employe
+    {
+        private string Nom;
+        private string Prenom;
+
+        public Employe(string Nom, string Prenom)
+        {
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+        }
+
+        public void profession() 
+        {
+        }
+    }
+}
---
+++ emprunt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class emprunt
+    {
+        private int Debut;
+        private int FinDate;
+
+        public emprunt(int Debut, int FinDate)
+        {
+            this.Debut = Debut;
+            this.FinDate = FinDate;
+        }
+    }
+}
