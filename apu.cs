8dbfeb8960ad4dba9bf176ff55fb26cbf0ab713f
FatimaElhaddad <116873467+FatimaElhaddad@users.noreply.github.com>
2023-01-21 02:08:46 +0100

    Add files via upload

---
+++ apu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class apu
+    {
+        private int ID;
+        private string Nom;
+
+        public apu(int id, string nom)
+        {
+            ID = id;
+            this.Nom = nom;
+        }
+}
